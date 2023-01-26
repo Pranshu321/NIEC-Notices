f8671b13306e9100085f4d3f33b5f1e85376f57e
Pranshu321 <pranshujain0111@gmail.com>
2023-01-26 22:40:25 +0530

    Reload added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Niec Notices
 - This is place where you find the all notices released by the **Northern India Enginnering College**.
 
-## [Live](https://niec.up.railway.app/)
+## [Live](https://niec.onrender.com)
 
 ## Old UI
 ![image](https://user-images.githubusercontent.com/86917304/186363236-54b4139b-8d5b-4f30-be96-54f9ea2fa503.png)
--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const app = require('express')();
-const express = require('express');
+const request = require("request");
+const cheerio = require("cheerio");
+const app = require("express")();
+const express = require("express");
 
-app.set("view engine", 'ejs');
-app.use(express.static('public'));
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 let linksarr = [];
 let tilte = [];
 let NoticeDate = [];
 request("https://adgitmdelhi.ac.in/notice/", (err, res, html) => {
-    linksarr = [];
-    if (err)
-        console.log("Error", err);
-    else {
-        let $ = cheerio.load(html);
-        let h1s = $(".post-content>a");
-        let time = $(".post-content>time");
-        for (let i = 0; i < h1s.length; i++) {
-            linksarr.push(h1s[i].attribs.href);
-            tilte.push(h1s[i].children[0].data);
-            NoticeDate.push(time[i].attribs.datetime.slice(0, 10));
-        }
-        // console.log(NoticeDate);
-    }
+	linksarr = [];
+	if (err) console.log("Error", err);
+	else {
+		let $ = cheerio.load(html);
+		let h1s = $(".post-content>a");
+		let time = $(".post-content>time");
+		for (let i = 0; i < h1s.length; i++) {
+			linksarr.push(h1s[i].attribs.href);
+			tilte.push(h1s[i].children[0].data);
+			NoticeDate.push(time[i].attribs.datetime.slice(0, 10));
+		}
+		// console.log(NoticeDate);
+	}
 });
 // console.log(tilte);
 
-
-app.get('/', (req, res) => {
-    res.render('home', { links: linksarr, title: tilte, Dates: NoticeDate });
+app.get("/", (req, res) => {
+	if (linksarr.length === 0) {
+		window.location.reload();
+	} else {
+		res.render("home", { links: linksarr, title: tilte, Dates: NoticeDate });
+	}
 });
 
 app.listen(process.env.PORT || 5500, () => {
-    console.log("ok");
+	console.log("ok");
 });
